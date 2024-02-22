61ccb2d01e5eeb740b4461273c03f9d277d8efa0
sarah chung <sarahchungwu@gmail.com>
2024-02-23 11:52:58 +1300

    change the method to async

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 
@@ -9,23 +10,25 @@ namespace API.Controllers
   [Route("api/[controller]")]
   public class ProductsController : ControllerBase
   {
-    private readonly StoreContext context;
+    private readonly StoreContext _context;
+
     public ProductsController(StoreContext context)
     {
-      this.context = context;
+      _context = context;
+
     }
 
     [HttpGet]
-    public ActionResult<List<Product>> GetProducts()
+    public async Task<ActionResult<List<Product>>> GetProducts()
     {
-      var products = context.Products.ToList();
-      return Ok(products);
+      return await _context.Products.ToListAsync();
     }
 
+
     [HttpGet("{id}")]
-    public ActionResult<Product> GetProduct(int id)
+    public async Task<ActionResult<Product>> GetProduct(int id)
     {
-      return context.Products.Find(id);
+      return await _context.Products.FindAsync(id);
     }
   }
 }
