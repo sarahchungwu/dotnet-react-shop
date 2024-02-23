6efe5f957099f745f6036d8011002f2cb9b6bb1c
sarah chung <sarahchungwu@gmail.com>
2024-02-23 14:38:47 +1300

    added the cors configuration to the API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from 'react'
+
 function App() {
+  const [products, setProducts] = useState([
+    { name: 'product1', price: 100.0 },
+    { name: 'product2', price: 100.0 },
+  ])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then((respone) => respone.json())
+      .then((data) => setProducts(data))
+  }, [])
+
+  function addProduct() {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        name: 'product' + (prevState.length + 1),
+        price: prevState.length * 100 + 100,
+      },
+    ])
+  }
   return (
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add product</button>
     </div>
   )
 }
