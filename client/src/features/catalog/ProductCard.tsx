00ab180f99604b2c0b07b3d9ab8f2f1ff9bb110e
sarah chung <sarahchungwu@gmail.com>
2024-02-24 10:13:55 +1300

    create the product details

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -9,6 +9,7 @@ import {
   CardHeader,
 } from '@mui/material'
 import { Product } from '../../app/models/product'
+import { Link } from 'react-router-dom'
 interface Props {
   product: Product
 }
@@ -45,7 +46,9 @@ export const ProductCard = ({ product }: Props) => {
       </CardContent>
       <CardActions>
         <Button size="small">Add to Cart</Button>
-        <Button size="small">View</Button>
+        <Button component={Link} to={`/catalog/${product.id}`} size="small">
+          View
+        </Button>
       </CardActions>
     </Card>
   )
