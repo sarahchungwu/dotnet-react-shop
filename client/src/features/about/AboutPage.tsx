652ff41babf4198c7e396d1e024bf379718a9c9d
sarah chung <sarahchungwu@gmail.com>
2024-02-28 18:43:41 +1300

    using anxios interceptors for error handling

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -10,31 +10,43 @@ export const AboutPage = () => {
       <ButtonGroup fullWidth>
         <Button
           variant="contained"
-          onClick={() => agent.TestErrors.get400Error()}
+          onClick={() =>
+            agent.TestErrors.get400Error().catch((error) => console.log(error))
+          }
         >
           Test 400 Error
         </Button>
         <Button
           variant="contained"
-          onClick={() => agent.TestErrors.get401Error()}
+          onClick={() =>
+            agent.TestErrors.get401Error().catch((error) => console.log(error))
+          }
         >
           Test 401 Error
         </Button>
         <Button
           variant="contained"
-          onClick={() => agent.TestErrors.get404Error()}
+          onClick={() =>
+            agent.TestErrors.get400Error().catch((error) => console.log(error))
+          }
         >
           Test 404 Error
         </Button>
         <Button
           variant="contained"
-          onClick={() => agent.TestErrors.get500Error()}
+          onClick={() =>
+            agent.TestErrors.get500Error().catch((error) => console.log(error))
+          }
         >
           Test 500 Error
         </Button>
         <Button
           variant="contained"
-          onClick={() => agent.TestErrors.getValidationError()}
+          onClick={() =>
+            agent.TestErrors.getValidationError().catch((error) =>
+              console.log(error)
+            )
+          }
         >
           Test Validation Error
         </Button>
