b2700e1f519576ee472c6d76be82a183a459874e
sarah chung <sarahchungwu@gmail.com>
2024-02-28 17:02:50 +1300

    create a react component to test the error

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,5 +1,44 @@
-import { Typography } from '@mui/material'
+import { Button, ButtonGroup, Container, Typography } from '@mui/material'
+import agent from '../../app/api/agent'
 
 export const AboutPage = () => {
-  return <Typography variant="h2">About page</Typography>
+  return (
+    <Container>
+      <Typography gutterBottom variant="h2">
+        Errors for testing purpose
+      </Typography>
+      <ButtonGroup fullWidth>
+        <Button
+          variant="contained"
+          onClick={() => agent.TestErrors.get400Error()}
+        >
+          Test 400 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => agent.TestErrors.get401Error()}
+        >
+          Test 401 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => agent.TestErrors.get404Error()}
+        >
+          Test 404 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => agent.TestErrors.get500Error()}
+        >
+          Test 500 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => agent.TestErrors.getValidationError()}
+        >
+          Test Validation Error
+        </Button>
+      </ButtonGroup>
+    </Container>
+  )
 }
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -8,7 +8,6 @@ import {
   TableRow,
   Typography,
 } from '@mui/material'
-import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Product } from '../../app/models/product'
@@ -22,7 +21,7 @@ export const ProductDetails = () => {
     id &&
       agent.Catalog.details(parseInt(id))
         .then((response) => setProduct(response))
-        .catch((error) => console.log(error))
+        .catch((error) => console.log(error.response))
         .finally(() => setLoading(false))
   }, [id])
 
