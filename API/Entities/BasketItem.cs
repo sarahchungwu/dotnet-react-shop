30fc50404de46fce5d798e8b885bf610eb5c1d0c
sarah chung <sarahchungwu@gmail.com>
2024-03-02 16:13:40 +1300

    created the basket entity

---
+++ API/Entities/BasketItem.cs
@@ -0,0 +1,18 @@
+
+
+
+
+namespace API.Entities
+{
+  public class BasketItem
+  {
+    public int Id { get; set; }
+
+    public int Quantity { get; set; }
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+
+
+
+  }
+}
