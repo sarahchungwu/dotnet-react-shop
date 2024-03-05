623882ecea6db64f02d4dbe144e4910fec78a6ca
sarah chung <sarahchungwu@gmail.com>
2024-03-05 22:22:30 +1300

    using the react context

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { Header } from './Header'
 import {
@@ -10,8 +10,24 @@ import {
 import { Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { useStoreContext } from '../context/StoreContext'
+import { getCookie } from '../util/util'
+import agent from '../api/agent'
+import { LoadingComponent } from './LoadingComponent'
 
 function App() {
+  const { setBasket } = useStoreContext()
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const buyerId = getCookie('buyerId')
+    if (buyerId) {
+      agent.Basket.get()
+        .then((basket) => setBasket(basket))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false))
+    }
+  }, [setBasket])
+
   const [darkMode, setDarkMode] = useState(false)
   const paletteType = darkMode ? 'dark' : 'light'
   const theme = createTheme({
@@ -25,6 +41,8 @@ function App() {
   function handleThemeChange() {
     setDarkMode(!darkMode)
   }
+
+  if (loading) return <LoadingComponent message="Initialising app..." />
   return (
     <ThemeProvider theme={theme}>
       <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
