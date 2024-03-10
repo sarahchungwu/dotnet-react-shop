cbef9dbcb9827d1126271dfab252e3008404b0c7
sarah chung <sarahchungwu@gmail.com>
2024-03-10 21:52:05 +1300

    set up the redux toolkit

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,5 +1,41 @@
-import { Typography } from '@mui/material'
+import { Button, ButtonGroup, Typography } from '@mui/material'
+
+import { decrement, increment } from './counterSlice'
+import { useAppSelector } from '../../app/store/configureStore'
+import { useDispatch } from 'react-redux'
 
 export const ContactPage = () => {
-  return <Typography variant="h2">Contact page</Typography>
+  const dispatch = useDispatch()
+  const { data, title } = useAppSelector((state) => state.counter)
+  return (
+    <>
+      <Typography gutterBottom variant="h3">
+        {title}
+      </Typography>
+      <Typography variant="h4">The data is: {data}</Typography>
+      <ButtonGroup>
+        <Button
+          onClick={() => dispatch(decrement(1))}
+          variant="contained"
+          color="error"
+        >
+          Decrement
+        </Button>
+        <Button
+          onClick={() => dispatch(increment(1))}
+          variant="contained"
+          color="primary"
+        >
+          Increment
+        </Button>
+        <Button
+          onClick={() => dispatch(increment(5))}
+          variant="contained"
+          color="secondary"
+        >
+          Increment by 5
+        </Button>
+      </ButtonGroup>
+    </>
+  )
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,12 +8,16 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router/Routes.tsx'
+import { Provider } from 'react-redux'
+import { store } from './app/store/configureStore.ts'
 import { StoreProvider } from './app/context/StoreContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StoreProvider>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </StoreProvider>
   </React.StrictMode>
 )
