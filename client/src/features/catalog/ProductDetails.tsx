fc995a50a648293b9619e270ea3716f13e7728a1
sarah chung <sarahchungwu@gmail.com>
2024-03-18 14:39:53 +1300

    fix error handling in the reducers

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -125,7 +125,7 @@ export const ProductDetails = () => {
               disabled={
                 item?.quantity === quantity || (!item && quantity === 0)
               }
-              loading={status.includes('pending' + item?.productId)}
+              loading={status.includes('pending')}
               onClick={handleUpdateCart}
               sx={{ height: '55px' }}
               color="primary"
--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -10,22 +10,22 @@ import { RootState } from '../../app/store/configureStore'
 const productsAdapter = createEntityAdapter<Product>()
 export const fetchProductsAsync = createAsyncThunk<Product[]>(
   'catalog/fetchProductsAsync',
-  async () => {
+  async (_, thunkAPI) => {
     try {
       return await agent.Catalog.list()
-    } catch (error) {
-      console.log(error)
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({ error: error.data })
     }
   }
 )
 
 export const fetchProductAsync = createAsyncThunk<Product, number>(
   'catalog/fetchProductAsync',
-  async (productId) => {
+  async (productId, thunkAPI) => {
     try {
       return await agent.Catalog.details(productId)
-    } catch (error) {
-      console.log(error)
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({ error: error.data })
     }
   }
 )
@@ -58,7 +58,8 @@ export const catalogSlice = createSlice({
       productsAdapter.upsertOne(state, action.payload)
       state.status = 'idle'
     })
-    builder.addCase(fetchProductAsync.rejected, (state) => {
+    builder.addCase(fetchProductAsync.rejected, (state, action) => {
+      console.log(action)
       state.status = 'idle'
     })
   },
