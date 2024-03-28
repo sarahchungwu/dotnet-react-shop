675d47f4a788138b0efdc4846dc9b545a1ca6b26
sarah chung <sarahchungwu@gmail.com>
2024-03-28 22:28:40 +1300

    added pagination helper classes

---
+++ API/RequestHelpers/MetaData.cs
@@ -0,0 +1,12 @@
+
+
+namespace API.RequestHelpers
+{
+  public class MetaData
+  {
+    public int CurrentPage { get; set; }
+    public int TotalPage { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+  }
+}
