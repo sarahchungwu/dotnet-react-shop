b9524c5eb078f49346f6d69879f292acc058b3d6
sarah chung <sarahchungwu@gmail.com>
2024-04-08 20:15:37 +1200

    set up the axios get request to use params

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -41,14 +41,15 @@ axios.interceptors.response.use(
   }
 )
 const request = {
-  get: (url: string) => axios.get(url).then(responseBody),
+  get: (url: string, params?: URLSearchParams) =>
+    axios.get(url, { params }).then(responseBody),
   post: (url: string, body: object) => axios.post(url, body).then(responseBody),
   put: (url: string, body: object) => axios.put(url, body).then(responseBody),
   delete: (url: string) => axios.delete(url).then(responseBody),
 }
 
 const Catalog = {
-  list: () => request.get('products'),
+  list: (params: URLSearchParams) => request.get('products', params),
   details: (id: number) => request.get(`products/${id}`),
   fetchFilters: () => request.get('products/filters'),
 }
--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -17,16 +17,34 @@ interface CatalogState {
 }
 
 const productsAdapter = createEntityAdapter<Product>()
-export const fetchProductsAsync = createAsyncThunk<Product[]>(
-  'catalog/fetchProductsAsync',
-  async (_, thunkAPI) => {
-    try {
-      return await agent.Catalog.list()
-    } catch (error: any) {
-      return thunkAPI.rejectWithValue({ error: error.data })
-    }
+
+function getAxiosParams(productParams: ProductParams) {
+  const params = new URLSearchParams()
+  params.append('pageNumber', productParams.pageNumber.toString())
+  params.append('pageSize', productParams.pageSize.toString())
+  params.append('orderBy', productParams.orderBy)
+  if (productParams.searchTerm)
+    params.append('searchTerm', productParams.searchTerm)
+  if (productParams.brands)
+    params.append('brands', productParams.brands.toString())
+
+  if (productParams.types)
+    params.append('types', productParams.types.toString())
+  return params
+}
+
+export const fetchProductsAsync = createAsyncThunk<
+  Product[],
+  void,
+  { state: RootState }
+>('catalog/fetchProductsAsync', async (_, thunkAPI) => {
+  const params = getAxiosParams(thunkAPI.getState().catalog.productParams)
+  try {
+    return await agent.Catalog.list(params)
+  } catch (error: any) {
+    return thunkAPI.rejectWithValue({ error: error.data })
   }
-)
+})
 
 export const fetchProductAsync = createAsyncThunk<Product, number>(
   'catalog/fetchProductAsync',
